39c0fd2af53d315e1779749f43d1090e9445d295
zurfyx <zurfyx@gmail.com>
2017-07-07 16:31:17 +0200

    demo kml

--- src/app/shared/services/kml.service.ts
+++ src/app/shared/services/kml.service.ts
@@ -10,6 +10,30 @@ export class KmlService {
    */
   journey() {
     // TODO.
-    return 'WIP';
+    const randomNumber = ~~(Math.random() * 10); // tslint:disable-line:no-bitwise
+    const demoKml = `<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Document>
+    <Placemark>
+      <name>Demo ${randomNumber}</name>
+      <description>
+        <![CDATA[
+          <html>
+          <head>
+            <style>html, body { margin: 0; padding: 0; }</style>
+          <body>
+            <h1>Demo</h1>
+          </body>
+        </html>
+        ]]>
+      </description>
+      <gx:balloonVisibility>0</gx:balloonVisibility>
+      <Point>
+        <coordinates>102,14</coordinates>
+      </Point>
+    </Placemark>
+  </Document>
+</kml>`;
+    return demoKml;
   }
 }
