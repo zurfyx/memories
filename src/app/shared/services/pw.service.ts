23823faf39af245270b17c5cb9cdbc1930353160
zurfyx <zurfyx@gmail.com>
2017-07-15 19:15:31 +0200

    createPw abstraction

--- src/app/shared/services/pw.service.ts
+++ src/app/shared/services/pw.service.ts
@@ -15,23 +15,15 @@ export class PwService {
   ) { }
 
   createPw(pw: Pw): Observable<Pw> {
-    return this.afAuth.authState.first().flatMap((user) => {
-      const userUid = user.uid;
-      const pwRef = this.afDatabase.database.ref(`physicalWeb/${userUid}`);
-      const pushPromise = pwRef.push(pw);
-      return Observable.fromPromise(pushPromise)
-        .map((snapshot: firebase.database.ThenableReference) => new Pw(snapshot));
-    })
+    return this.userPwRef()
+      .flatMap(pwRef => Observable.from(pwRef.push(pw))
+      .map((snapshot: firebase.database.ThenableReference) => new Pw(snapshot)));
   }
 
   readPws(): Observable<Pw[]> {
     return this.userPwRef()
-      .flatMap((pwRef) => (
-        this.afDatabase.list(pwRef)
-      ))
-      .map((snapshot: any[]) => (
-        snapshot.map((pwValues) => new Pw(pwValues))
-      ));
+      .flatMap(pwRef => this.afDatabase.list(pwRef))
+      .map((snapshot: any[]) => snapshot.map(pwValues => new Pw(pwValues)));
   }
 
   private userPwRef(): Observable<firebase.database.Reference> {
