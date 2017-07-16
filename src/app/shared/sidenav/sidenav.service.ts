b606453a23f034d254bf55d75bcb7ae73853612a
zurfyx <zurfyx@gmail.com>
2017-07-16 17:22:54 +0200

    close sidenav after navigating

--- src/app/shared/sidenav/sidenav.service.ts
+++ src/app/shared/sidenav/sidenav.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs/Rx';
 
 @Injectable()
 export class SidenavService {
   isMobileNavbarOpen: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(
+    private router: Router,
+  ) {
+    // Close sidenav after navigating.
+    router.events.subscribe(() => this.isMobileNavbarOpen.next(false));
+  }
 }
