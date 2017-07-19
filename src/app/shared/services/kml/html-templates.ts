2194fcb0f02c6ad9ccc799e3b402d3140fc7f217
zurfyx <zurfyx@gmail.com>
2017-07-19 16:23:53 +0200

    kml refactor - moved service into its own folder xml templates and html templates are stored in individual documents

---
+++ src/app/shared/services/kml/html-templates.ts
@@ -0,0 +1,77 @@
+import ellipsis from 'text-ellipsis';
+
+export default {
+  bubble: ({imageUrl, dateText, ownerDisplayName, description}) => `
+    ${imageUrl ? `<img class="banner" src="${imageUrl}" />` : ''}
+    <div class="separator"></div>
+    <div class="row tcenter">
+      <span class="col2 date">${dateText}</span>
+      <span class="col2 author">${ownerDisplayName}</span>
+    </div>
+    <div class="separator"></div>
+    <pre class="description">${ellipsis(description, 450)}</pre>
+    <div class="logo tcenter">
+      <i class="fa fa-globe" aria-hidden="true"></i>
+    </div>
+    <div class="copyright tcenter">Geographical Memories @ 2017</div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      width: 600px;
+      font-family: 'Helvetica', Arial, sans-serif;
+    }
+
+    .separator {
+      margin-top: 15px;
+      border-top: 1px solid #ddd;
+      margin-bottom: 15px;
+    }
+
+    .row:after {
+      display: block;
+      content: '';
+      clear: both;
+    }
+
+    .col2 {
+      width: 50%;
+      float: left;
+    }
+
+    .tcenter {
+      text-align: center;
+    }
+
+    .inline > * {
+      flex: 1;
+    }
+
+    .banner {
+      width: 100%;
+      max-height: 420px;
+    }
+
+    .date {
+      display: block;
+    }
+
+    pre.description {
+      font-family: inherit;
+      text-align: justify;
+      text-justify: inter-word;
+      white-space: pre-wrap;
+    }
+
+    .logo {
+      color: #4386fc;
+    }
+
+    .copyright {
+      margin-top: 10px;
+      font-size: 0.8em;
+    }
+    </style>
+  `,
+};
--- src/app/shared/services/kml/kml.service.ts
+++ src/app/shared/services/kml/kml.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import ellipsis from 'text-ellipsis';
 import { Observable } from 'rxjs/Rx';
 
-import { Story, User } from '../models';
-import { UserService } from './user.service';
+import { Story, User } from '../../models';
+import { UserService } from '../user.service';
+import html from './html-templates';
+import xml from './xml-templates';
 
 @Injectable()
 export class KmlService {
@@ -48,7 +49,7 @@ export class KmlService {
       </Point>
       <description>
         <![CDATA[
-          ${KML_TEMPLATES.bubble({
+          ${html.bubble({
             imageUrl: story.coverURL,
             dateText: this.datePipe.transform(story.dateStart),
             ownerDisplayName: user.displayName,
@@ -119,11 +120,7 @@ export class KmlService {
   }
 
   wrap(content: string, includeHeaders = true): string {
-    const withHeaders = `<?xml version="1.0" encoding="UTF-8"?>
-            <kml xmlns="http://www.opengis.net/kml/2.2"
-                 xmlns:gx="http://www.google.com/kml/ext/2.2">
-              <Document>${content}</Document>
-            </kml>`;
+    const withHeaders = xml.document(content);
     const minified = this.minify(includeHeaders ? withHeaders : content);
     return this.minify(minified);
   }
@@ -134,78 +131,3 @@ export class KmlService {
   }
 }
 
-const KML_TEMPLATES = {
-  bubble: ({imageUrl, dateText, ownerDisplayName, description}) => `
-    ${imageUrl ? `<img class="banner" src="${imageUrl}" />` : ''}
-    <div class="separator"></div>
-    <div class="row tcenter">
-      <span class="col2 date">${dateText}</span>
-      <span class="col2 author">${ownerDisplayName}</span>
-    </div>
-    <div class="separator"></div>
-    <pre class="description">${ellipsis(description, 450)}</pre>
-    <div class="logo tcenter">
-      <i class="fa fa-globe" aria-hidden="true"></i>
-    </div>
-    <div class="copyright tcenter">Geographical Memories @ 2017</div>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-    html, body {
-      margin: 0;
-      padding: 0;
-      width: 600px;
-      font-family: 'Helvetica', Arial, sans-serif;
-    }
-
-    .separator {
-      margin-top: 15px;
-      border-top: 1px solid #ddd;
-      margin-bottom: 15px;
-    }
-
-    .row:after {
-      display: block;
-      content: '';
-      clear: both;
-    }
-
-    .col2 {
-      width: 50%;
-      float: left;
-    }
-
-    .tcenter {
-      text-align: center;
-    }
-
-    .inline > * {
-      flex: 1;
-    }
-
-    .banner {
-      width: 100%;
-      max-height: 420px;
-    }
-
-    .date {
-      display: block;
-    }
-
-    pre.description {
-      font-family: inherit;
-      text-align: justify;
-      text-justify: inter-word;
-      white-space: pre-wrap;
-    }
-
-    .logo {
-      color: #4386fc;
-    }
-
-    .copyright {
-      margin-top: 10px;
-      font-size: 0.8em;
-    }
-    </style>
-  `,
-};
