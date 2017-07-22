008ec47f93968c0871a2db745a9ef0676da995e6
zurfyx <zurfyx@gmail.com>
2017-07-22 19:21:10 +0200

    read comments

--- src/app/story/detail/comment/story-comment.component.ts
+++ src/app/story/detail/comment/story-comment.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import {
+  User,
+  UserService,
   Story,
+  Comment,
+  CommentService,
 } from '../../../shared';
 
 @Component({
@@ -12,7 +16,33 @@ import {
 export class StoryCommentComponent implements OnInit {
   @Input() story: Story;
 
-  constructor() { }
+  comments: Comment[];
+  users: { [id: string]: User } = {}; // Serves as a cache. So that repeated users aren't fetched
+                                      // several times (if they have published more than a comment).
 
-  ngOnInit() { }
+  constructor(
+    private commentService: CommentService,
+    private userService: UserService,
+  ) { }
+
+  ngOnInit() {
+    this.commentService.readComments(this.story.$key).subscribe((comments: Comment[]) => {
+      // Comments are given from first to last. We are going to display them last to first.
+      const commentsReversed = comments.reverse();
+      this.comments = commentsReversed;
+
+      // Gather user data associated with these comments.
+      comments.forEach(comment => this.refreshUser(comment.owner));
+    });
+  }
+
+  refreshUser(userId: string) {
+    if (this.users[userId]) {
+      return;
+    }
+    this.users[userId] = new User({}); // Async is going to take a while until it assigns the real values.
+    this.userService.readUser(userId, ['displayName', 'photoURL']).first().subscribe((user: User) => {
+      this.users[userId] = user;
+    });
+  }
 }
