d25b504dfe3f9d222e708ac8094c6fe961a0b81e
zurfyx <zurfyx@gmail.com>
2017-07-31 17:23:25 +0200

    db -> delete journeys

--- firebase/__tests__/journeys.spec.ts
+++ firebase/__tests__/journeys.spec.ts
@@ -10,92 +10,74 @@ const AUTH_HACKER = {
   uid: '666',
 };
 
-describe('Stories', () => {
+describe('Journeys', () => {
   it('should be able to create as a signed in user', () => {
     const data = {
       users: {
         '123': {
-          displayName: '.'
-        }
-      },
-      journeys: {
-        'abc': {
-          owner: '123',
+          displayName: '.',
         }
       }
     };
-    const NEW_STORY = {
-      journey: 'abc',
+    const NEW_JOURNEY = {
       owner: '123',
-      updatedAt: { '.sv': 'timestamp' },
       title: '🍭',
-    }
+      updatedAt: { '.sv': 'timestamp' },
+    };
     const db = targaryen.database(rules, data).as(AUTH_USER);
-    const { allowed } = db.write('stories/xyz', NEW_STORY);
+    const { allowed } = db.write('journeys/abc', NEW_JOURNEY);
     expect(allowed).toBe(true);
   });
 
   it('should be able to edit as the owner', () => {
     const data = {
       users: {
-        '123': {
+        '123':  {
           displayName: '.',
         }
       },
       journeys: {
         'abc': {
           owner: '123',
-        }
-      },
-      stories: {
-        'xyz': {
-          journey: 'abc',
-          owner: '123',
+          title: '🍭',
           updatedAt: 0,
-          title: '🍭'
         }
       }
     };
     const db = targaryen.database(rules, data);
 
     const asUser = db.as(AUTH_USER);
-    const { allowed: allowedUser } = asUser.write('stories/xyz/title', '🍬');
+    const { allowed: allowedUser } = asUser.write('journeys/abc/title', '🍬');
     expect(allowedUser).toBe(true);
 
     const asHacker = db.as(AUTH_HACKER);
-    const { allowed: allowedHacker } = asHacker.write('stories/xyz/title', 'HACKED');
+    const { allowed: allowedHacker } = asHacker.write('journeys/abc/title', 'HACKED');
     expect(allowedHacker).toBe(false);
   });
 
   it('should be able to delete as the owner', () => {
     const data = {
       users: {
-        '123': {
-          displayName: '.'
+        '123':  {
+          displayName: '.',
         }
       },
       journeys: {
         'abc': {
           owner: '123',
-        }
-      },
-      stories: {
-        'xyz': {
-          journey: 'abc',
-          owner: '123',
+          title: '🍭',
           updatedAt: 0,
-          title: '🍭'
         }
       }
     };
     const db = targaryen.database(rules, data);
 
     const asUser = db.as(AUTH_USER);
-    const { allowed: allowedUser } = asUser.write('stories/xyz', null);
+    const { allowed: allowedUser } = asUser.write('journeys/abc', null);
     expect(allowedUser).toBe(true);
 
     const asHacker = db.as(AUTH_HACKER);
-    const { allowed: allowedHacker } = asHacker.write('stories/xyz', null);
+    const { allowed: allowedHacker } = asHacker.write('journeys/abc', null);
     expect(allowedHacker).toBe(false);
   });
 });
