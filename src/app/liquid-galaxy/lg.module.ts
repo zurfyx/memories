fd486ecc5da4a6c0dc2c33809ce265753bc840c2
zurfyx <zurfyx@gmail.com>
2017-08-09 17:22:15 +0200

    added ids to documentation headings (references)

--- src/app/liquid-galaxy/lg.module.ts
+++ src/app/liquid-galaxy/lg.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 import { LgComponent } from './lg.component';
 import { LgRoutingModule } from './lg-routing.module';
 
 @NgModule({
   imports: [
+    Ng2PageScrollModule,
     LgRoutingModule,
   ],
   exports: [],
