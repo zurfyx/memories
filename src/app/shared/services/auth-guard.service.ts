1dd35bb0ce10317de0011bf87c3d559c4304cd65
zurfyx <zurfyx@gmail.com>
2017-08-11 17:37:44 +0200

    restrict journey new to signed in users only

--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -7,10 +7,17 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
+    private router: Router,
     private afAuth: AngularFireAuth,
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.afAuth.authState.first().map((user: firebase.User) => !!user);
+    return this.afAuth.authState.first().map((user: firebase.User) => {
+      if (!user) {
+        this.router.navigate(['/']);
+        return false;
+      }
+      return true;
+    });
   }
 }
